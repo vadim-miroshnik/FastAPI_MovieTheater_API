a2636a4fe6a2babb9d2fa3e6e8280090dcee3e98
dmitriyshinkarev <dsh.dev33@gmail.com>
2022-06-06 16:50:43 +0300

    structures tests

---
+++ tests/functional/testdate/data_genre.py
@@ -0,0 +1,10 @@
+genre_data: list[dict] = [
+        {"id": "3d8d9bf5-0d90-4353-88ba-4ccc5d2c07ff", "name": 	"Action"},
+        {"id": "120a21cf-9097-479e-904a-13dd7198c1dd", "name": 	"Adventure"},
+        {"id": "b92ef010-5e4c-4fd0-99d6-41b6456272cd", "name": 	"Fantasy"},
+        {"id": "6c162475-c7ed-4461-9184-001ef3d9f26e", "name": 	"Sci-Fi"},
+        {"id": "1cacff68-643e-4ddd-8f57-84b62538081a", "name": 	"Drama"},
+        {"id": "56b541ab-4d66-4021-8708-397762bff2d4", "name": 	"Music"},
+        {"id": "237fd1e4-c98e-454e-aa13-8a13fb7547b5", "name": 	"Romance"},
+        {"id": "526769d7-df18-4661-9aa6-49ed24e9dfd8", "name": 	"Thriller"}
+]
